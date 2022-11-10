54933ebe1ac73d0b0104ae9327a3a27ea238d7cc
LoScarro <davisca2000@gmail.com>
2022-11-10 16:47:12 +0100

    ora alla creazione delle code non schedula più un job in ogni coda

--- mmn_queue.py
+++ mmn_queue.py
@@ -23,47 +23,49 @@ class MMN(Simulation):
         # check if there is at least one server
 
         super().__init__()
-        self.running = [] # if not None, the id of the running job
+        self.running = []  # if not None, the id of the running job
         self.queues = []
         self.arrivals = {}  # dictionary mapping job id to arrival time
         self.completions = {}  # dictionary mapping job id to completion time
-        self.lambd = lambd  #probability of a new job entry
-        self.n = n  #number of servers
-        self.mu = mu    #probability of finishing a job
+        self.lambd = lambd  # probability of a new job entry
+        self.n = n  # number of servers
+        self.mu = mu  # probability of finishing a job
         self.arrival_rate = lambd * n
         self.completion_rate = mu
-        self.d = int(round(n/100*d, 0))    #percentage of queues to be monitored
+        self.d = int(round(n/100*d, 0))  # percentage of queues to be monitored
         if self.d == 0:
             self.d = 1
         for i in range(n):
             self.running.append(None)
             self.queues.append(collections.deque())  # FIFO queue of the system
-            self.schedule(expovariate(lambd), Arrival(i, 0))   #first job
+        self.schedule(expovariate(lambd), Arrival(0, 0))  # first job
 
     def marketplace(self):
         min_index = randint(0, self.n-1)
         min = self.queue_len(min_index)
 
-        for i in range(self.d - 1):  # searching for the empiest queue among the d monitored
+        for i in range(self.d):  # searching for the empiest queue among the d monitored
             temp_index = randint(0, self.n-1)
             temp = self.queue_len(temp_index)
             if min > temp:
                 min = temp
                 min_index = temp_index
-        #print(min_index)       DEBUG
-        #print(self.sampling())     DEBUG
+        #print(min_index)
+        #print(self.sampling(), "marketplace")
         return min_index
 
-    def schedule_arrival(self, job_id): 
-        # schedule the arrival following an exponential distribution, 
+    def schedule_arrival(self, job_id):
+        # schedule the arrival following an exponential distribution,
         # to compensate the number of queues the arrival time should depend also on "n
-        self.schedule(expovariate(self.lambd), Arrival(self.marketplace(), job_id))
+        self.schedule(expovariate(self.lambd),
+                      Arrival(self.marketplace(), job_id))
 
-    def schedule_completion(self, server_id, job_id): #TODO find a way to remember the queue
+    # TODO find a way to remember the queue
+    def schedule_completion(self, server_id, job_id):
         # schedule the time of the completion event
         self.schedule(expovariate(self.completion_rate), Completion(server_id, job_id))
-        
-    #@property
+
+    # @property
     def queue_len(self, server_id):
         return (self.running[server_id] is None) + len(self.queues[server_id])
 
@@ -73,11 +75,13 @@ class MMN(Simulation):
             sample_list.append(self.queue_len(i))
         return sample_list
 
+
 class Arrival(Event):
 
     def __init__(self, server_id, job_id):
         super().__init__(server_id)
         self.id = job_id
+        #print(self.id, server_id, "arrival")
 
     def process(self, sim: MMN):
         # set the arrival time of the job
@@ -92,10 +96,12 @@ class Arrival(Event):
         # schedule the arrival of the next job (this is where we create jobs)
         sim.schedule_arrival(self.id + 1)
 
+
 class Completion(Event):
     def __init__(self, server_id, job_id):
         super().__init__(server_id)
         self.id = job_id  # currently unused, might be useful when extending
+        #print(self.id, server_id, "complition")
 
     def process(self, sim: MMN):
         assert sim.running[self.server_id] is not None
@@ -110,35 +116,40 @@ class Completion(Event):
         else:
             sim.running[self.server_id] = None
 
+
 def main():
-    #command line option
+    # command line option
     parser = argparse.ArgumentParser()
     parser.add_argument('--lambd', type=float, default=0.7)
     parser.add_argument('--mu', type=float, default=1)
-    parser.add_argument('--max-t', type=float, default=5)
+    parser.add_argument('--max-t', type=float, default=1_000_000)
     parser.add_argument('--n', type=int, default=10)
     parser.add_argument('--csv', help="CSV file in which to store results")
-    parser.add_argument('--sample_rate', type=int, default=-1000, help="queue lenght sampling rate based in simulation time")#queue lenght sampling
-    parser.add_argument('--d', type=int, default=70, help="percentage of servers to be queried")
+    parser.add_argument('--sample_rate', type=int, default=1000,
+                        help="queue lenght sampling rate based in simulation time")  # queue lenght sampling
+    parser.add_argument('--d', type=int, default=70,
+                        help="percentage of servers to be queried")
     args = parser.parse_args()
     assert args.d > 0 and args.d <= 100
-    #initialization of MMN simulation
+    # initialization of MMN simulation
     sim = MMN(args.lambd, args.mu, args.n, args.d)
     sim.run(args.max_t, args.sample_rate)
     completions = sim.completions
 
     for t, leng in sim.sample_list:
-        print("time: ", round(t, 0) , ",\tnumber of events in the queue " , leng)
+        print("time: ", round(t, 0), ",\tnumber of events in the queue ", leng)
 
-    W = (sum(completions.values()) - sum(sim.arrivals[job_id] for job_id in completions)) / len(completions)
+    W = (sum(completions.values()) -
+         sum(sim.arrivals[job_id] for job_id in completions)) / len(completions)
     print(f"Average time spent in the system: {W}")
-    print(f"Theoretical expectation for random server choice: {1 / (1 - args.lambd)}")
+    print(
+        f"Theoretical expectation for random server choice: {1 / (1 - args.lambd)}")
     # lambda = 1 lead to an division by 0, lambda > 1 lead to a negative expectation
 
-    #-------------------------------------------------------------------------------------------------#
-    
-    #-------------------------------------------------------------------------------------------------#
-    
+    # -------------------------------------------------------------------------------------------------#
+
+    # -------------------------------------------------------------------------------------------------#
+
     if args.csv is not None:
         with open(args.csv, 'a', newline='') as f:
             writer = csv.writer(f)
