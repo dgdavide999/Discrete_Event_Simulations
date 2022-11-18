dece3bd3c274f934ff2bf74a299a198f9f1206aa
dgdavide999 <dgdavide999@gmail.com>
2022-11-18 11:04:33 +0100

    graphic plot multiplots, makefile siplified

--- graphics.py
+++ graphics.py
@@ -3,6 +3,13 @@ import re
 
 document = "out.txt"
 
+def sample():
+    #sampled queue's lenght
+    for line in sim_data[5:]:
+        line = line.split(" ")
+        #frist queue's lenght in line[9]
+        insert(line[9:]) #insert sampled queue's lenght in a list of dictionaries
+
 def insert(sample):
     #create a new dict
     samp_dict = dict()
@@ -19,51 +26,51 @@ def insert(sample):
     samp_dict = {k: v/n for k, v in samp_dict.items()}
     percentage.append(samp_dict)
 
+def average(final_dict):
+    #average of the queues' lenght
+    for d in percentage:
+        for k, v in d.items():
+            if k in final_dict:
+                final_dict[k] += v
+            else:
+                final_dict[k] = v
+    final_dict = {k: v/len(percentage) for k, v in final_dict.items()}
+    return final_dict
 
-try:
-    file = open(document, 'r')
-    #simulation data
-    sim_data = file.readlines(0)
-    sim_metadata = sim_data[0].split(" ")
-    n = int(sim_metadata[5])
-    W = sim_data[2].split(" ")
-    W = W[6]
-    expectation = sim_data[3].split(" ")
-    expectation = expectation[6]
+#Main
+colour = ['b', 'r', 'darkorange', 'gold', 'g']
+for lambd in ["0.5", "0.9", "0.95", "0.99"]:
+    try:
+        file = open("out.txt_"+lambd, 'r')
+        #simulation data
+        sim_data = file.readlines(0)
+        sim_metadata = sim_data[0].split(" ")
+        n = int(sim_metadata[5])
+        W = sim_data[2].split(" ")
+        W = W[6]
+        expectation = sim_data[3].split(" ")
+        expectation = expectation[6]
 
-    
-    #sampled queue's lenght
-    percentage = []
-    for line in sim_data[5:]:
-        line = line.split(" ")
-        #frist queue's lenght in line[9]
-        insert(line[9:]) #insert sampled queue's lenght in a list of dictionaries
-    
-except Exception as e:
-    print(e)
-finally:
-    file.close()
+        percentage = []
+        sample()
+        
+    except Exception as e:
+        print(e)
+    finally:
+        file.close()
 
-final_dict = dict()
-#average of the queues' lenght
-for d in percentage:
-    for k, v in d.items():
-        if k in final_dict:
-            final_dict[k] += v
-        else:
-            final_dict[k] = v
-final_dict = {k: v/len(percentage) for k, v in final_dict.items()}
+    final_dict = dict()
+    final_dict = average(final_dict)
 
-print("\n\n\n", final_dict)
-
+    print("\n\n\n", final_dict)
+    plt.plot(final_dict.keys(), final_dict.values(), colour.pop(), label="lambda = "+ lambd)
+plt.legend(loc = "upper right")
 plt.title(sim_metadata[0] + " " + sim_metadata[1]+ " "+sim_metadata[2])
 plt.xlabel('queues lenght')
 plt.ylabel('percentage fullness')
 plt.xlim(0, 14)
-
 lenght_tiks = [0, 2, 4, 6, 8, 10, 12 ,14]
 percentage_tiks = [0, 0.2, 0.4, 0.6, 0.8, 1]
-plt.plot(final_dict.keys(), final_dict.values())
 plt.xticks(lenght_tiks)
 plt.yticks(percentage_tiks)
 plt.show()
